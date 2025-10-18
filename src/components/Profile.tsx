ee3375dbcbb56355d30a6cae1d7a080a300347fe
104838522 <104838522@student.swin.edu.au>
2025-10-18 20:39:56 +1100

    Fix alt text for profile image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,7 +2,7 @@
 function Profile({name, imgsrc = "https://placehold.co/150", role}:{name: string, imgsrc?: string, role: string}){
     return(
         <div className="profile">
-            <img src={imgsrc} alt="Profile photo"></img>
+            <img src={imgsrc} alt="Profile"></img>
             <h2>{name}</h2>
             <h3>{role}</h3>
         </div>
