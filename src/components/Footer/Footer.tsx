4456db13fbf0c5c6656e8aef05dab6c237aa9840
Violet Qi <violetqi81@gmail.com>
2025-10-20 17:35:15 +1100

    Create Footer CSS

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
+import styles from "./Footer.module.scss"
+
 function Footer(){
     const currentYear: number = new Date().getFullYear();
     return(
-        <footer>
+        <footer className={styles.footer}>
             <p>&copy;Violet Qi {currentYear}</p>
+            <p className={styles.pright}>SDE40006 Project Group Wed-16:30-MD-G6</p>
         </footer>
     )
 }
