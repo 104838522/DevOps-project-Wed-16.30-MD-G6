dc0354cd8a06b3f7e019e6cedb9f8da4a2308a5c
Violet Qi <violetqi81@gmail.com>
2025-10-20 13:44:05 +1100

    Create HeroBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import HeroBlock from "./components/HeroBlock/HeroBlock";
 import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HeroBlock/>
       <Profile/>
       <Footer/>
     </div>
---
+++ src/components/HeroBlock/HeroBlock.tsx
@@ -0,0 +1,10 @@
+function HeroBlock(){
+
+    return(
+        <div>
+            <img src={require('../../images/softdev.jpg')}/>
+        </div>
+    )
+}
+
+export default HeroBlock
