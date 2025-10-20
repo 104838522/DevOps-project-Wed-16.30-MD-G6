a1d842e6d771e35723c9dc79d1c5ee11bc2d4200
Violet Qi <violetqi81@gmail.com>
2025-10-20 14:28:23 +1100

    ContentBlock Framework

--- src/components/ContentBlock/ContentBlock.tsx
+++ src/components/ContentBlock/ContentBlock.tsx
@@ -1,11 +1,26 @@
 
+type ContentBlockProps = {
+    title: string,
+    content: string,
+    image: NodeRequire,
+    left: boolean
+}
 
-function ContentBlock(){
-    return(
-        <div>
-            
-        </div>
-    );
+function ContentBlock({title, content, image, left}:ContentBlockProps){
+    if (left){
+        return(
+            <div>
+                
+            </div>
+        );
+    } else {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+
 }
 
 export default ContentBlock;
