44154ecf1e665b003dbd29bb7235a1defd77607c
Violet Qi <violetqi81@gmail.com>
2025-10-20 17:54:14 +1100

    add subtitle to heroblock

--- src/components/HeroBlock/HeroBlock.tsx
+++ src/components/HeroBlock/HeroBlock.tsx
@@ -5,6 +5,7 @@ function HeroBlock(){
     return(
         <div className={styles.container}>
             <h1>Software Deployment and Evolution</h1>
+            <h2>Group Assignment - SDE40006</h2>
             <img src={require('../../images/softdev.jpg')}/>
         </div>
     )
